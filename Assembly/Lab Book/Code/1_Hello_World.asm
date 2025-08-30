c5f56d8d39860dd35fbe9f40a7f89daeef5ba305
jawadahmedkh <jawadfarman786@gmail.com>
2025-08-31 00:11:23 +0500

    fgfg

---
+++ Assembly/Lab Book/Code/1_Hello_World.asm
@@ -0,0 +1,12 @@
+.686
+INCLUDE Irvine32.inc 
+.data
+    msg BYTE "Hello, from Assembly!",0
+.code
+    Main PROC
+        lea edx, msg
+        Call Clrscr
+        Call WriteString
+        EXIT
+    Main ENDP
+    END Main
